f4db7d56a69be1aad87e84b012630f2126cfadb0
hv5 <per.evje@gmail.com>
2018-12-22 17:01:38 +0100

    Initial commit

---
+++ Engine.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace demo
+{
+    class Engine
+    {
+        public Engine()
+        {
+        }
+
+        public double Multiply(double a, double b){
+            return a*b;
+        }
+
+        public double Divide(double a, double b){
+            return a/b;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Engine eng = new Engine();
+            var v = eng.Multiply(23.0,56.0);
+
+            Console.WriteLine("Hello World: {0:0.00}", v);
+        }
+    }
+}
